f90fc4510308f0ebf2a31c7a5e3ffa9fbc0df4d7
Valiantsin Baliakin <Valiantsin_Baliakin@epam.com>
2020-02-24 14:13:26 +0300

    add clean version promiseTask.js

--- promiseTask.js
+++ promiseTask.js
@@ -1,18 +1,31 @@
-let positiveSleep = milliseconds => {
-    return new Promise(resolve => {
-        setTimeout(() => resolve(), milliseconds);
-    });
-};
+function isPositive(number) {
+    return number >= 0;
+}
 
-let addWithDelay = (firstNumber, secondNumber) => {
-    if (firstNumber > 0 && secondNumber > 0) {
-        return positiveSleep(1000).then((firstNumber, secondNumber) => (firstNumber + secondNumber));
-    } else {
-        return new Promise((resolve, reject) => {
-            reject(console.log('Error! Some parameter is negative number!'));
+function verifyNumbers (...args) {
+    return args.every(isPositive);
+}
+
+function addWithDelay(...args) {
+    if (verifyNumbers(...args) === true) {
+        let positivePromise = new Promise(function (resolve, reject) {
+            setTimeout(() => resolve(args.reduce((a, b) => a + b), 1000));
         });
+        positivePromise.then(
+            result => console.log(result),
+            error => console.log(error)
+        );
+    } else {
+        let negativePromise = new Promise(function (resolve, reject) {
+            reject('Here are (one OR more) negative numbers');
+        });
+        negativePromise.then(
+            result => console.log(result),
+            error => console.log(error)
+        );
     }
-};
+    return args.reduce((a, b) => a + b);
+}
 
-console.log(addWithDelay(2, 5));
-addWithDelay(-2, 5);
+addWithDelay(4,8,-6);
+addWithDelay(4,8,13);
