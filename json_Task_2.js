65041c06ad200eafb32ce694c426fdf822213d49
Valiantsin Baliakin <Valiantsin_Baliakin@epam.com>
2020-02-25 18:01:05 +0300

    add draft of json_Task_2.js

--- json_Task_2.js
+++ json_Task_2.js
@@ -1,24 +1,58 @@
 const fs = require('fs'); // импортируем модуль fs в проект
+
 function verifyPropertiesInJSONFile() {
     fs.readFile('jsonFile.json', (error, data) => {
         if (error) {
             console.error(error);
         } else {
-            let dataFromJsonFile = data.toString();
-            console.log(dataFromJsonFile);
-            let convertedDataInObject = JSON.parse(dataFromJsonFile);
-            for (let item in convertedDataInObject) {
-                console.log(convertedDataInObject[item]);
-                // if ((typeof(convertedDataInObject[value]) === boolean) && (typeof(convertedDataInObject[item]) === boolean)) {
-                //         console.log('OK');
-                // }
+            let object = JSON.parse(data);
+            let wordForCompare = 'FiRst';
+            let flagForCompare = true;
+            let lengthForCompare = 8;
+            let word = 'string';
+            let counter = 0;
+            let numberOfStringElements = 8;
+            let array = object.parameters;
+            let lowerBorder = 5;
+            let upperBound = 13;
+
+            let verifyFlag = typeof (object.flag); // boolean
+            let verifyMyPromises = Array.isArray(object.myPromises); // true
+            let verifyElement = typeof (object.element); // object
+            let verifyScreenshot = (object.screenshot === null); // true
+            let verifyElementText = typeof (object.elementText); // string
+            let verifyAllElementsText = (object.allElementsText.indexOf('const') !== -1); // true
+
+            let verifyCounter = object.counter > 10; // false
+            let verifyCommon = object.config === 'Common'; // false
+            let verifyConst = object.const.toUpperCase() === wordForCompare.toUpperCase(); // true
+            let verifyParametersDraft = ((Array.isArray(array) === flagForCompare) && (array.length === lengthForCompare)); // предварительно TRUE
+            if (verifyParametersDraft) {
+                array.forEach(element => {
+                    if (typeof (element) === typeof (word)) {
+                        counter++;
+                    }
+                });
             }
-            let checkType = true;
-        
-            console.log("\n" + typeof(checkType));
-            console.log(typeof(convertedDataInObject));
+            let verifyParametersClean = (counter === numberOfStringElements); // true
+            let verifyDescription = ((object.description.length > lowerBorder) && (object.description < upperBound)); // false
+
+            console.log(verifyFlag, verifyMyPromises, verifyElement, verifyScreenshot, verifyElementText, verifyAllElementsText);
+            console.log(verifyCounter, verifyCommon, verifyConst, verifyParametersClean, verifyDescription);
         }
     });
 }
 
 verifyPropertiesInJSONFile();
+
+// "flag" - boolean
+// 	"myPromises" - array
+// 	"element" - object
+// 	"screenshot" - null
+// 	"elementText" - string
+// 	"allElementsText" - contain "const" in string
+// 	"counter" - more than 10
+// 	"config" - equal "Common"
+// 	"const" - equal "FiRst" (case insensitive)
+// 	"parameters" - array with length 8 and all elements are string
+// 	"description" - string with length more than 5 but less than 13
