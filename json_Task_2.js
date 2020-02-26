518e63be94c4b0d090a8b6913e44b03a8fda9b9f
Valiantsin Baliakin <Valiantsin_Baliakin@epam.com>
2020-02-26 14:21:09 +0300

    add almost finally version json_Task_2.js

--- json_Task_2.js
+++ json_Task_2.js
@@ -1,50 +1,89 @@
-const fs = require('fs'); // импортируем модуль fs в проект
-
-function verifyPropertiesInJSONFile() {
-    fs.readFile('jsonFile.json', (error, data) => {
-        if (error) {
-            console.error(error);
-        } else {
-            let object = JSON.parse(data);
-            let wordForCompare = 'FiRst';
-            let flagForCompare = true;
-            let lengthForCompare = 8;
-            let word = 'string';
-            let counter = 0;
-            let numberOfStringElements = 8;
-            let array = object.parameters;
-            let lowerBorder = 5;
-            let upperBound = 13;
-
-            let verifyFlag = typeof (object.flag); // boolean
-            let verifyMyPromises = Array.isArray(object.myPromises); // true
-            let verifyElement = typeof (object.element); // object
-            let verifyScreenshot = (object.screenshot === null); // true
-            let verifyElementText = typeof (object.elementText); // string
-            let verifyAllElementsText = (object.allElementsText.indexOf('const') !== -1); // true
-
-            let verifyCounter = object.counter > 10; // false
-            let verifyCommon = object.config === 'Common'; // false
-            let verifyConst = object.const.toUpperCase() === wordForCompare.toUpperCase(); // true
-            let verifyParametersDraft = ((Array.isArray(array) === flagForCompare) && (array.length === lengthForCompare)); // предварительно TRUE
-            if (verifyParametersDraft) {
-                array.forEach(element => {
+function verifyJSONFile() {
+    const fs = require('fs'); // импортируем модуль fs в проект
+    const jsonFile = fs.readFileSync('jsonFile.json');
+    const object = JSON.parse(jsonFile);
+    let wordForCompare = 'FiRst',
+        flagForCompare = true,
+        lengthForCompare = 8,
+        word = 'string',
+        counter = 0,
+        numberOfStringElements = 8,
+        lowerBound = 5,
+        upperBound = 13,
+        numberForCompare = 10,
+        counterOfMistakes = 0,
+        quantityOfFields = 0;
+    const verifyFlag = (parameter) => {
+            return typeof parameter == 'boolean';
+        }, // boolean -> true
+        verifyMyPromises = (myPromises) => {
+            return Array.isArray(myPromises);
+        }, // array -> true
+        verifyElement = (element) => {
+            return element instanceof Object; // object -> true НО ЭТО НЕ СОВСЕМ ОБЪЕКТ
+        },
+        verifyScreenshot = (screenshot) => {
+            return Object.is(screenshot, null);
+        }, // null -> true
+        verifyElementText = (elementText) => {
+            return typeof (elementText) == 'string'; // string -> true
+        },
+        verifyAllElementsText = (allElementsText) => {
+            return allElementsText.indexOf('const') !== -1; // содержит строку 'const' -> true
+        },
+        verifyCounter = (counter, numberForCompare) => {
+            return counter > numberForCompare; // > numberForCompare -> true (или False)
+        },
+        verifyCommon = (config) => {
+            return config === 'Common'; // false
+        },
+        verifyConst = (word, wordForCompare) => {
+            return word.toUpperCase() === wordForCompare.toUpperCase(); // true
+        },
+        verifyParametersClean = (parameters) => {
+            if ((Array.isArray(parameters) === flagForCompare) && (parameters.length === lengthForCompare)) { // предварительно TRUE
+                parameters.forEach(element => {
                     if (typeof (element) === typeof (word)) {
                         counter++;
                     }
                 });
             }
-            let verifyParametersClean = (counter === numberOfStringElements); // true
-            let verifyDescription = ((object.description.length > lowerBorder) && (object.description < upperBound)); // false
-
-            console.log(verifyFlag, verifyMyPromises, verifyElement, verifyScreenshot, verifyElementText, verifyAllElementsText);
-            console.log(verifyCounter, verifyCommon, verifyConst, verifyParametersClean, verifyDescription);
+            return counter === numberOfStringElements; // true
+        },
+        verifyDescription = (description) => {
+            return (description.length > lowerBound) && (description.length < upperBound); // false
+        };
+    const verifyJSON = {
+        flag: verifyFlag(object.flag),
+        myPromises: verifyMyPromises(object.myPromises),
+        element: verifyElement(object.element),
+        screenshot: verifyScreenshot(object.screenshot),
+        elementText: verifyElementText(object.elementText),
+        allElementsText: verifyAllElementsText(object.allElementsText),
+        counter: verifyCounter(object.counter, numberForCompare),
+        config: verifyCommon(object.config),
+        constParameter: verifyConst(object.const, wordForCompare),
+        parameters: verifyParametersClean(object.parameters),
+        description: verifyDescription(object.description)
+    };
+    fs.writeFileSync('jsonFileErrors.txt', 'The condition is not satisfied in the element: \n');
+    for (let key in verifyJSON) {
+        quantityOfFields = Object.keys(verifyJSON).length;
+        if (verifyJSON[key] == true) {
+            counterOfMistakes++;
+        } else {
+            for (let item in object) {
+                if (key == item) {
+                    fs.appendFileSync('jsonFileErrors.txt', key + ': ' + object[item] + '\n ');
+                }
+            }
         }
-    });
+    }
+    let result = counterOfMistakes === quantityOfFields ? 'All is OKAY' : 'See file jsonFileErrors.txt';
+    console.log(result);
 }
 
-verifyPropertiesInJSONFile();
-
+verifyJSONFile();
 // "flag" - boolean
 // 	"myPromises" - array
 // 	"element" - object
