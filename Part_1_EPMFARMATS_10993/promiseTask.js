4db1c7ec2055209309d1133f60361abb2c1e8b3c
Valiantsin Baliakin <valentinakaryuk@gmail.com>
2020-03-02 11:11:21 +0300

    add second version of resolve promiseTask.js

--- Part_1_EPMFARMATS_10993/promiseTask.js
+++ Part_1_EPMFARMATS_10993/promiseTask.js
@@ -27,5 +27,21 @@ function addWithDelay(...args) {
     return args.reduce((a, b) => a + b);
 }
 
+// second version
+
+function addWithDelaySecond(...args) {
+    return new Promise ((resolve, reject) => {
+         args.every((arrayElement) => arrayElement > 0) ? setTimeout(() => {
+            resolve(args.reduce((counter, currentElement) => counter + currentElement));
+        }, 1000) : reject ('Here are (one OR more) negative numbers');
+    });
+}
+
+addWithDelaySecond(4,8,-6).then (result => {
+    console.log(result);
+}).catch (error => {
+    console.log(error);
+});
+
 addWithDelay(4,8,-6);
 addWithDelay(4,8,13);
